21579efd7420ee101773cd704c5fffa8cfed4e93
Henning <henning.holgersen@gmail.com>
2024-03-14 20:04:17 +0100

    glot

--- dbtai/utils.py
+++ dbtai/utils.py
@@ -1,6 +1,7 @@
 import yaml
 import os
 import appdirs
+import sqlglot
 
 def get_config():
     configdir = appdirs.user_data_dir("dbtai", "dbtai")
@@ -8,3 +9,27 @@ def get_config():
     with open(os.path.join(configdir, "config.yaml"), "r") as f:
         return yaml.load(f, Loader=yaml.FullLoader)
     
+
+
+def find_ctes(expression, ctes=[]):
+    if isinstance(expression, sqlglot.exp.CTE):
+        ctes.append(expression)
+    if hasattr(expression, "args"):
+      for child in expression.args.values():
+          if isinstance(child, list):
+              for item in child:
+                  if hasattr(item, "args"):
+                    find_ctes(item, ctes)
+          else:
+              find_ctes(child, ctes)
+    return ctes
+
+def final_selected_columns(sql_query):
+    parsed = sqlglot.parse_one(sql_query)
+    ctes = find_ctes(parsed)
+
+    columns = [expr.alias_or_name for expr in ctes[-1].this.expressions]
+    return columns
+
+
+parsed = sqlglot.parse_one(sql_query)
